b95bcbad4f4bfa675a6256713b6a85a6bbaf869f
Tijn Van de Velde <vandeveldetijn@gmail.com>
2021-03-19 12:09:00 +0100

    update

--- backend/meldingenController.php
+++ backend/meldingenController.php
@@ -2,16 +2,39 @@
 
 //Variabelen vullen
 $attractie = $_POST['attractie'];
-$capaciteit = $_POST['capaciteit']; 
+$type = $_POST['type'];
+$capaciteit = $_POST['capaciteit'];
+if(isset($_POST['prioriteit']))
+{
+    $prioriteit = true;
+}
+else
+{
+    $prioriteit = false;
+}
 $melder = $_POST['melder'];
+$overig = $_POST['overig'];
 
-echo $attractie . " / " . $capaciteit . " / " . $melder;
+echo $attractie . " / " . $type . " / " . $capaciteit . " / " . $prioriteit . " / " . $type . " / " . $overig; 
 
 //1. Verbinding
 require_once 'conn.php';
 
 //2. Query
+$query = "INSERT INTO meldingen (attractie, type, capaciteit, prioriteit, melder, overige_info)
+VALUES(:attractie, :type, :capaciteit, :prioriteit, :melder, :overige_info)";
 
 //3. Prepare
+$statement = $conn->prepare($query);
 
 //4. Execute
+$statement->execute([
+    ":attractie" => $attractie,
+    ":type" => $type,
+    ":capaciteit" => $capaciteit,
+    ":prioriteit" => $prioriteit,
+    ":melder" => $melder,
+    ":overige_info" => $overig,
+]);
+
+header("Location: ../meldingen/index.php?msg=Melding opgeslagen");
